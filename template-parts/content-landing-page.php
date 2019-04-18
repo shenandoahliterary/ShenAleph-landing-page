4aa4bfb5a2a974819942db4233f540b79cb03cc0
jeffreybarry <iamjeffbarry@gmail.com>
2019-04-18 15:45:56 -0400

    Update content-landing-page.php

--- template-parts/content-landing-page.php
+++ template-parts/content-landing-page.php
@@ -30,9 +30,9 @@ $upload_path =  $uploads['baseurl'];
 		$landingpage_loop = new WP_Query($landingpage_args);
 				while ($landingpage_loop->have_posts()) : $landingpage_loop->the_post();
 				 ?>
-				<p>
+				<p class="TOC-quote">
 
-					<span class="TOC-quote"><?php the_content();  ?></span>
+					<?php the_content();  ?>
 
 			</p>
 		<?php endwhile;
