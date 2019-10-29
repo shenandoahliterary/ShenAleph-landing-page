8e818b57d9860ada765048d0c39492d9852c3e7f
jeffreybarry <iamjeffbarry@gmail.com>
2019-10-29 13:09:08 -0400

    Update issues and about links in header.php

--- header.php
+++ header.php
@@ -43,8 +43,8 @@
 		</div>
 			<div class="col-md-5">
 				<div class="shen-nav d-flex flex-column flex-sm-row justify-content-end" >
-					<a class="nav-item nav-link" href="https://shenandoahliterary.org/682/about/">About</a>
-					<a class="nav-item nav-link" href="https://shenandoahliterary.org/682/issues/">Issues</a>
+					<a class="nav-item nav-link" href="https://shenandoahliterary.org/about/">About</a>
+					<a class="nav-item nav-link" href="https://shenandoahliterary.org/issues/">Issues</a>
 					<a class="nav-item nav-link" href="https://shenandoahliterary.org/681/submit/">Submit</a>
 					<a class="nav-item nav-link" href="https://shenandoahliterary.org/682/category/feature">The Peak</a>
 				</div>
