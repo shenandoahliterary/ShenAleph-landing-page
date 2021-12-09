a40f3ee9cec189f26aea8e30e3ba78748df285f3
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2021-12-09 12:58:52 -0500

    Commit all

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+ShenAleph-landing-page
+===
+
+This is a landing page theme for Shenandoah.
+
+Use at your own risk
---
+++ front-page.php
@@ -0,0 +1,44 @@
+<?php
+/**
+* Template Name: Front Page Template
+*
+* Description: A page template that provides a key component of WordPress as a CMS
+* by meeting the need for a carefully crafted introductory page. The front page template
+* in Twenty Twelve consists of a page content area for adding text, images, video --
+* anything you'd like -- followed by front-page-only widgets in one or two columns.
+
+ *
+ * @package ShenAleph
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		if ( have_posts() ) :
+
+
+
+
+							/* Start the Loop */
+							while ( have_posts() ) :
+								the_post();
+								/*
+								 *
+								 *
+								 * Pulls in content
+								 */
+								get_template_part( 'template-parts/content', 'landing-page' );
+							endwhile;
+
+						endif;
+						?>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
