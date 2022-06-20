94f784b1d361d8aa20ffa715fbf2a61310835b07
Jeff Barry <10522393+jeffreybarry@users.noreply.github.com>
2022-06-20 14:21:36 -0400

    Update page-issues.php
    added 712 issue

--- page-issues.php
+++ page-issues.php
@@ -26,6 +26,13 @@ get_header();
 						<h1 class="entry-header entry-title">Issues</h1>
 						<div class="card-deck">
 					<!-- insert latest issues here -->
+					<div class="card w-300"><a href="https://shenandoahliterary.org/712">
+						<img class="card-img-top" src="https://shenandoahliterary.org/712/files/2022/06/245x300.jpg" alt="Vol 71, Issue 1">
+						<div class="card-body">
+							<h5 class="card-title">Volume 71 Issue 2</h5>
+						</div>
+					</a>
+					</div>
 					<div class="card w-300"><a href="https://shenandoahliterary.org/711">
 						<img class="card-img-top" src="https://shenandoahliterary.org/711/files/2021/12/Sharma-Manjari-art-300w.jpg" alt="Vol 71, Issue 1">
 						<div class="card-body">
