6278174cfc43ff0492980de20e39ad5bdadadbde
jjham1 <julieham03@gmail.com>
2023-03-06 19:04:08 -0500

    updating footer

--- footer.php
+++ footer.php
@@ -13,22 +13,33 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer container">
-		<hr>
-		<div class="site-info row">
-			<div class="col-md-12">
-				<p class="text-right"><a href="https://www.facebook.com/ShenandoahLiterary"><i class="fab fa-facebook-square"></i></a> <a href="https://www.instagram.com/shenandoah_literary"><i class="fab fa-instagram"></i></a> <a href="https://twitter.com/ShenandoahWLU"><i class="fab fa-twitter"></i></a>   </p>
-<img class="img-fluid float-left shen-logo-footer" src="https://shenandoahliterary.org/681/files/2018/12/shen-w175.png"><img class="img-fluid wlu-logo" src="https://shenandoahliterary.org/681/files/2018/12/wlu-w300.png">
-<img class="img-fluid float-right" src="https://shenandoahliterary.org/681/files/2018/12/clmp.png">
-			</div>
-<div class="row">
-	<div class="col-md-12">
-	<p class="copyright">&copy; 2018&mdash;2019 Shenandoah</p>
-</div>
+	<footer>
 
-		</div><!-- .site-info -->
+		<section style="background-color: rgb(103,173,168)">
+			<div class="row footer-above">
+
+				<div class="col images">
+					<a href="https://www.facebook.com/ShenandoahLiterary"><i class="fab fa-facebook-square" style="padding-left:15px"></i></a>
+					<a href="https://www.instagram.com/shenandoah_literary"><i class="fab fa-instagram"></i></a>
+					<a href="https://twitter.com/ShenandoahWLU"><i class="fab fa-twitter"></i></a>
+				</div>
+
+				<div class="col text-end">
+					<a class="clmp" style="margin-top:-10px" href = "https://www.clmp.org/"><img src="https://shenandoahliterary.org/681/files/2018/12/clmp.png" href = "https://www.clmp.org/"></a>
+					<br>
+					<a class="wl-logo" style="padding-top:5px; padding-right: 5px" href = "https://www.wlu.edu/"><img src="https://shenandoahliterary.org/681/files/2018/12/wlu-w300.png"></a>
+				</div>
+
+			</div>
+
+			<div class="row">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/shen-header-new.svg" style="z-index:1" class="img-fluid" itemprop="logo">
+			</div>
+		</section>
+
 	</footer><!-- #colophon -->
-</div><!-- #page  also ends Bootstrap container-->
+
+	</div><!-- #page  also ends Bootstrap container-->
 
 <?php wp_footer(); ?>
 
